b5d69c0bcfcf9c1136c09c6110873d907554f5d2
AldoMekic <aldomekic@gmail.com>
2024-04-29 20:14:05 +0200

    Expanded the Footer component with links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { MyContext } from '../context/my-context';  // Ensure this path is correct
 import '../styles/Footer.css';
 
 const Footer = () => {
+  const { user } = useContext(MyContext);
+
   return (
     <footer className="footer">
-      <p className="footer-text">Library&amp;Archive</p>
+      <div className="footer-links">
+        <Link to="/">Home</Link>
+        {user ? (
+          <Link to="/profile">Profile</Link>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </div>
+      <div className="footer-text-container">
+        <p className="footer-text">Library&amp;Archive</p>
+      </div>
     </footer>
   );
 };
